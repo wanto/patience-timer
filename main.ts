1b8f7bc03c895b104a20a0ef67428be95a4f64de
Wanja Lechner <wanja.lechner@gmail.com>
2020-11-08 20:04:29 +0100

    implemented most of the logic

---
+++ main.ts
@@ -0,0 +1,155 @@
+class Game {
+    player1_time: number;
+    player2_time: number;
+    bonus_time: number;
+    turn_started: Date;
+    turn: string;
+
+    player1_nextmove_button: HTMLButtonElement;
+    player2_nextmove_button: HTMLButtonElement;
+    player1_endturn_button: HTMLButtonElement;
+    player2_endturn_button: HTMLButtonElement;
+
+    player1_player_timer: HTMLDivElement;
+    player2_player_timer: HTMLDivElement;
+
+    start_game(): void {
+        game.init();
+
+        var input = (<HTMLInputElement>document.getElementById("playtime-input")).value.split("/");
+        this.bonus_time = +input[1];
+        this.player1_time = (+input[0]) * 60;
+        this.player2_time = (+input[0]) * 60;
+
+        this.turn = "starting";
+        this.player1_nextmove_button.disabled = false;
+        this.player1_nextmove_button.classList.remove("disabled-button");
+        this.player2_nextmove_button.disabled = false;
+        this.player2_nextmove_button.classList.remove("disabled-button");
+        this.player1_endturn_button.disabled = true;
+        this.player2_endturn_button.disabled = true;
+
+        setInterval(() => {
+            this.refresh_timer();
+        }, 1000);
+    }
+
+    refresh_timer(): void {
+
+        if(this.turn === "starting"){
+            this.player1_player_timer.childNodes[1].textContent = this.player1_time/60 + ":" + (this.player1_time%60 < 10 ? "0" : "" ) + this.player1_time%60;
+            this.player2_player_timer.childNodes[1].textContent = this.player2_time/60 + ":" + (this.player2_time%60 < 10 ? "0" : "" ) + this.player2_time%60;
+        }
+        
+        if(this.turn === "player1"){
+            var diff:number = ((new Date().getTime() - this.turn_started.getTime()) / 1000) - this.bonus_time;
+            var bonus:number; var timer:number;
+
+            if(diff < 0){
+                bonus = Math.trunc(Math.abs(diff));
+                timer = this.player1_time;
+            }else{
+                bonus = 0;
+                timer = Math.trunc(this.player1_time - diff);
+            }
+
+            this.player1_player_timer.childNodes[1].textContent = Math.trunc(timer/60) + ":" + (timer%60 < 10 ? "0" : "" ) + Math.trunc(timer%60);
+            this.player1_player_timer.childNodes[3].textContent = "" + bonus;
+            this.player2_player_timer.childNodes[3].textContent = "0";
+        }
+        if(this.turn === "player2"){
+            var diff:number = ((new Date().getTime() - this.turn_started.getTime()) / 1000) - this.bonus_time;
+            var bonus:number; var timer:number;
+
+            if(diff < 0){
+                bonus = Math.trunc(Math.abs(diff));
+                timer = this.player2_time;
+            }else{
+                bonus = 0;
+                timer = Math.trunc(this.player2_time - diff);
+            }
+            this.player2_player_timer.childNodes[1].textContent = Math.trunc(timer/60) + ":" + (timer%60 < 10 ? "0" : "" ) + Math.trunc(timer%60);
+            this.player2_player_timer.childNodes[3].textContent = "" + bonus;
+            this.player1_player_timer.childNodes[3].textContent = "0";     
+        }
+    }
+
+    set_turn_player1 () {
+        if(this.turn === "player2"){
+            var diff:number = ((new Date().getTime() - this.turn_started.getTime()) / 1000) - this.bonus_time;
+            this.player2_time -= diff > 0 ? diff : 0;
+        }
+        this.turn_started = new Date();
+        this.turn = "player1";
+
+        this.player1_nextmove_button.disabled = false;
+        this.player1_nextmove_button.classList.remove("disabled-button");
+        this.player1_endturn_button.disabled = false;
+        this.player1_endturn_button.classList.remove("disabled-button");
+
+        this.player2_nextmove_button.disabled = true;
+        this.player2_nextmove_button.classList.add("disabled-button");
+        this.player2_endturn_button.disabled = true;
+        this.player2_endturn_button.classList.add("disabled-button");
+
+        this.refresh_timer();
+    }
+
+    set_turn_player2 () {
+        if(this.turn === "player1"){
+            var diff:number = ((new Date().getTime() - this.turn_started.getTime()) / 1000) - this.bonus_time;
+            this.player1_time -= diff > 0 ? diff : 0;
+        }
+        this.turn_started = new Date();
+        this.turn = "player2";
+
+        this.player2_nextmove_button.disabled = false;
+        this.player2_nextmove_button.classList.remove("disabled-button");
+        this.player2_endturn_button.disabled = false;
+        this.player2_endturn_button.classList.remove("disabled-button");
+
+        this.player1_nextmove_button.disabled = true;
+        this.player1_nextmove_button.classList.add("disabled-button");
+        this.player1_endturn_button.disabled = true;
+        this.player1_endturn_button.classList.add("disabled-button");
+
+        this.refresh_timer();
+    }
+    next_move () {
+
+        var diff:number = ((new Date().getTime() - this.turn_started.getTime()) / 1000) - this.bonus_time;
+        if(diff > 0){
+            if(this.turn === "player1")
+                this.player1_time -= diff;
+            if(this.turn === "player2")
+                this.player2_time -= diff;
+        }
+        this.turn_started = new Date();
+        this.refresh_timer();
+    }
+    nextmove_handler_player1 () {
+        if(this.turn === "starting")
+            this.set_turn_player1 ();
+        else
+            this.next_move();
+    }
+    nextmove_handler_player2 () {
+        if(this.turn === "starting")
+            this.set_turn_player2 ();
+        else
+            this.next_move();
+    }
+
+
+    init() {
+        this.player1_nextmove_button = <HTMLButtonElement>document.getElementById("player1-nextmove-button");
+        this.player2_nextmove_button = <HTMLButtonElement>document.getElementById("player2-nextmove-button");
+        this.player1_endturn_button = <HTMLButtonElement>document.getElementById("player1-endturn-button");
+        this.player2_endturn_button = <HTMLButtonElement>document.getElementById("player2-endturn-button");
+
+        this.player1_player_timer = <HTMLDivElement>document.getElementById("player1-player-timer");
+        this.player2_player_timer = <HTMLDivElement>document.getElementById("player2-player-timer");
+    }
+}
+
+var game = new Game();
